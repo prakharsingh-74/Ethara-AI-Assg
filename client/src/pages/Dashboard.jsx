9eac3dfb2c175c3e037fb4b1ce1b775b06389808
prakharsingh-74 <prakharsingh7014@gmail.com>
2026-05-07 16:44:06 +0530

    Bug fixes

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -205,7 +205,7 @@ const TaskTable = ({ tasks }) => {
 
       <td className='py-2'>
         <div className='flex'>
-          {task?.team.map((m, index) => (
+          {task?.team?.map((m, index) => (
             <div
               key={index}
               className={clsx(
